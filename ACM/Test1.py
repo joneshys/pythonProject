c7cf0e89c13b80932e0d1174dc4120ab1646ae8f
Jose Miguel Benitez <joneshys@gmail.com>
2021-03-08 18:14:48 -0500

    Eliminación de file test

--- ACM/Test1.py
+++
@@ -1,3 +0,0 @@
-size = [12,58,89,45]
-
-print('La suma del peso de todos los archivos es:'+str(sum(size))+' Y el Average es de: '+str(sum(size)/len(size)))
--- Delete_Backup_EC2/test.py
+++
@@ -1,3 +0,0 @@
-import collections
-z = ['blue', 'red', 'blue', 'yellow', 'blue', 'red']
-print(collections.Counter(z))
