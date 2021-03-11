938a090ba46ff728f1c1ad2dfe385d3f22416224
Jose Miguel Benitez <joneshys@gmail.com>
2021-03-11 08:25:04 -0500

    Se modifica List_Cloudwatch_Alarm.py para la creación de CSV

--- Cloudwatch/List_Cloudwatch_Alarm.py
+++ Cloudwatch/List_Cloudwatch_Alarm.py
@@ -1,13 +1,23 @@
 import boto3
+import csv
 from datetime import datetime
-#clientes = ['QXSSDDATLANTICOPROD', 'QXSSDDBELLOPROD', 'QXSSDDCALIPROD', 'QXHUNITIPROD', 'QXIMPTOSVALLEPROD', 'QXSSDDITAGUIPROD', 'QXSSDDPEREIRAPROD', 'QXINTRUNTPROD', 'QXBIPROD','QXPROD', 'QXCOMPARENDERASSOMOSPROD', 'QXSSDDSABANETAPROD']
-clientes = ['QXPROD']
+clientes = ['QXSSDDATLANTICOPROD', 'QXSSDDBELLOPROD', 'QXSSDDCALIPROD', 'QXHUNITIPROD', 'QXIMPTOSVALLEPROD', 'QXSSDDITAGUIPROD', 'QXSSDDPEREIRAPROD', 'QXINTRUNTPROD', 'QXBIPROD','QXPROD', 'QXCOMPARENDERASSOMOSPROD', 'QXSSDDSABANETAPROD']
+#clientes = ['QXPROD']
 Ahora = datetime.now().date()
+
+f4 = open("List_Alarm.csv", "a",newline='')
+cvs_w = csv.writer(f4)
+f4.close()
+
+
 for count in range(len(clientes)):
     profile = clientes[count]
     session = boto3.session.Session(region_name="us-east-1", profile_name=profile)
     ec2_re = session.resource(service_name="cloudwatch", region_name="us-east-1")
     ec2_cli = session.client("cloudwatch", region_name="us-east-1")
+
+    f4 = open("List_Alarm.csv", "a", newline='')
+    cvs_w = csv.writer(f4)
 
     paginator = ec2_cli.get_paginator('describe_alarms')
     #for response1 in paginator.paginate(StateValue='ALARM'):
@@ -17,6 +27,7 @@ for count in range(len(clientes)):
             for Alarm_Metric in Alarm_Metrics:
                 AlarmName = Alarm_Metric['AlarmName']
                 print(profile, AlarmName, 'INSUFFICIENT_DATA', Ahora)
+                cvs_w.writerow([profile, AlarmName, 'INSUFFICIENT_DATA', Ahora])
 
     for response1 in paginator.paginate(StateValue='ALARM'):
         if response1['MetricAlarms'] != []:
@@ -24,3 +35,6 @@ for count in range(len(clientes)):
             for Alarm_Metric in Alarm_Metrics:
                 AlarmName = Alarm_Metric['AlarmName']
                 print(profile, AlarmName, 'ALARM', Ahora)
+                cvs_w.writerow([profile, AlarmName, 'ALARM', Ahora])
+
+    f4.close()
