03f57a2056d1930249b82161080be2e9b7962f7d
Jose Miguel Benitez <joneshys@gmail.com>
2021-03-17 06:51:12 -0500

    Eliminación

---
+++ Secret_Manager/test+.py
@@ -0,0 +1,5 @@
+diccionario = {"username": "uqxadmin", "password": "%d}]XKlQzU&D<b~v8z$1f0:{429)vn", "engine": "oracle", "host": "proimpto.ckgnx7waruhw.us-east-1.rds.amazonaws.com", "port": 52896, "dbname": "PROIMPTO", "dbInstanceIdentifier": "proimpto"}
+print(diccionario)
+
+for k,v in diccionario.items():
+    print('%s -> %s'%(k,v))
