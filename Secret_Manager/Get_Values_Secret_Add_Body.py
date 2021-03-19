d572d8ed563f0e8e3e85afb5e0dcdb096ca30757
Jose Miguel Benitez <joneshys@gmail.com>
2021-03-19 17:07:46 -0500

    Se modifica para atachar ya sea en el cuerpo del correo o archivo.

--- Secret_Manager/Get_Values_Secret_Add_Body.py
+++ Secret_Manager/Get_Values_Secret_Add_Body.py
@@ -1,9 +1,3 @@
-diccionario = {"username": "uqxadmin", "password": "%d}]XKlQzU&D<b~v8z$1f0:{429)vn", "engine": "oracle", "host": "proimpto.ckgnx7waruhw.us-east-1.rds.amazonaws.com", "port": 52896, "dbname": "PROIMPTO", "dbInstanceIdentifier": "proimpto"}
-#print(diccionario)
-
-#for k,v in diccionario.items():
-#    print('%s -> %s'%(k,v))
-
 import boto3
 import smtplib
 from email.mime.multipart import MIMEMultipart
@@ -16,6 +10,9 @@ cloudwatch_cli = session.client(service_name='appstream', region_name='us-east-1
 ec2_cli = session.client(service_name='ec2', region_name='us-east-1')
 Secret = session.client(service_name='secretsmanager', region_name='us-east-1')
 IAMClient = session.client(service_name='iam', region_name='us-east-1')
+
+#Secret = boto3.client('secretsmanager')
+#IAMClient = boto3.client('iam')
 
 AccountAlias = IAMClient.list_account_aliases()['AccountAliases'][0].upper()
 #print(AccountAlias)
@@ -37,16 +34,15 @@ for k,v in my_dict.items():
     print(list_1)
     List_Format_Secret_Values.append(list_1)
 
-mail_content = AccountAlias+'\n'+List_Format_Secret_Values[1]+'\n'+List_Format_Secret_Values[2]+'\n'+List_Format_Secret_Values[3]+'\n'+List_Format_Secret_Values[4]+'\n'+List_Format_Secret_Values[5]+'\n'+List_Format_Secret_Values[6]
+mail_content = AccountAlias+'\n'+List_Format_Secret_Values[0]+'\n'+List_Format_Secret_Values[1]+'\n'+List_Format_Secret_Values[2]+'\n'+List_Format_Secret_Values[3]+'\n'+List_Format_Secret_Values[4]+'\n'+List_Format_Secret_Values[5]+'\n'+List_Format_Secret_Values[6]
 sender_address = 'jose.benitez@quipux.com'
 sender_pass = 'Neruda87$'
-receiver_address = "joneshys@gmail.com,jose.benitez@quipux.com"
+receiver_address = 'jose.benitez@quipux.com,fernando.bonilla@quipux.com'
 
 message = MIMEMultipart()
 message['From'] = sender_address
 message['To'] = receiver_address
 message['Subject'] = 'Sending of secret values from database RDS of the account'+' '+AccountAlias
-
 
 message.attach(MIMEText(mail_content, 'plain'))
 
--- Secret_Manager/Get_Values_Secret_Add_File.py
+++ Secret_Manager/Get_Values_Secret_Add_File.py
@@ -1,8 +1,13 @@
 import boto3
 import smtplib
+import ast
+import email, smtplib, ssl
+
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import ast
+from email.mime.base import MIMEBase
+from email import encoders
+
 
 session = boto3.session.Session(profile_name='QXIMPTOSVALLEPROD')
 ec2_re = session.resource(service_name='ec2', region_name='us-east-1')
@@ -10,9 +15,6 @@ cloudwatch_cli = session.client(service_name='appstream', region_name='us-east-1
 ec2_cli = session.client(service_name='ec2', region_name='us-east-1')
 Secret = session.client(service_name='secretsmanager', region_name='us-east-1')
 IAMClient = session.client(service_name='iam', region_name='us-east-1')
-
-#Secret = boto3.client('secretsmanager')
-#IAMClient = boto3.client('iam')
 
 AccountAlias = IAMClient.list_account_aliases()['AccountAliases'][0].upper()
 #print(AccountAlias)
@@ -34,22 +36,35 @@ for k,v in my_dict.items():
     print(list_1)
     List_Format_Secret_Values.append(list_1)
 
-mail_content = AccountAlias+'\n'+List_Format_Secret_Values[1]+'\n'+List_Format_Secret_Values[2]+'\n'+List_Format_Secret_Values[3]+'\n'+List_Format_Secret_Values[4]+'\n'+List_Format_Secret_Values[5]+'\n'+List_Format_Secret_Values[6]
+f = open("test.txt", "w")
+mail_content_1 = AccountAlias+'\n'+List_Format_Secret_Values[0]+'\n'+List_Format_Secret_Values[1]+'\n'+List_Format_Secret_Values[2]+'\n'+List_Format_Secret_Values[3]+'\n'+List_Format_Secret_Values[4]+'\n'+List_Format_Secret_Values[5]+'\n'+List_Format_Secret_Values[6]+'\n'+'--------------------------'
+f.write(mail_content_1)
+f.close()
+
 sender_address = 'jose.benitez@quipux.com'
 sender_pass = 'Neruda87$'
-receiver_address = 'juan.villa@quipux.com'
+receiver_address = "jose.benitez@quipux.com,juan.villa@quipux.com"
 
 message = MIMEMultipart()
 message['From'] = sender_address
 message['To'] = receiver_address
 message['Subject'] = 'Sending of secret values from database RDS of the account'+' '+AccountAlias
-
-message.attach(MIMEText(mail_content, 'plain'))
+mail_content = 'Sending of secret values from database RDS of the account AWS'
+#message.attach(MIMEText(mail_content, 'plain'))
+message.attach(MIMEText(mail_content,'plain'))
+filename = 'test.txt'
+with open(filename, 'rb') as attachment:
+    part = MIMEBase("application", "octet-stream")
+    part.set_payload(attachment.read())
+encoders.encode_base64(part)
+part.add_header("Content-Disposition",f"attachment; filename= {filename}")
+message.attach(part)
+text = message.as_string()
 
 session = smtplib.SMTP('smtp.gmail.com', 587)
 session.starttls()
 session.login(sender_address, sender_pass)
 text = message.as_string()
-session.sendmail(sender_address, receiver_address, text)
+session.sendmail(sender_address, receiver_address.split(','), text)
 session.quit()
 print('Mail Sent')
